90fde7c2dcaf01101291f4a485399c689d8c81a5
Corwin Marsh <corwinm@users.noreply.github.com>
2022-07-09 21:22:08 -0700

    Add prisma test with local container

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,20 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { PrismaClient } from "@prisma/client";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const prisma = new PrismaClient();
+  const count = await prisma.personTest.count();
+  return json({ count });
+};
+
 export default function Index() {
+  const { count } = useLoaderData();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1 className="font-serif">Welcome to Remix</h1>
+      <p>Total test count: {count}</p>
       <ul>
         <li>
           <a
